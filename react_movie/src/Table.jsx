3a946fcaf0e9d99f3bda27e2d1abf7e26fd7a8d1
Ashish-Prakash <ashishprakash52433@gmail.com>
2022-03-26 20:19:01 +0530

    pagination up

--- react_movie/src/Table.jsx
+++ react_movie/src/Table.jsx
@@ -28,7 +28,11 @@ class Table extends React.Component {
       }
     });
     let totalPages = Math.ceil(filteredMovies.length / 4);
-    let DisplayedMovies = filteredMovies.slice(0, 4);
+    let startIdx = (this.state.currentPage - 1) * 4;
+    let endIdx = Math.min(filteredMovies.length, this.state.currentPage * 4);
+    // console.log(startIdx);
+    // console.log(endIdx);
+    let DisplayedMovies = filteredMovies.slice(startIdx, endIdx);
     return (
       <>
         <div className="row">
@@ -92,73 +96,6 @@ class Table extends React.Component {
       </>
     );
   };
-  // let allmovies = props.moviedata;
-  // let currentmovie = props.selected;
-  // // console.log(allmovies);
-  // // console.log(currentmovie);
-
-  // let filteredMovies = allmovies.filter((el) => {
-  //   if (currentmovie === "All Genre") {
-  //     return el;
-  //   } else if (el.genre.name === currentmovie) {
-  //     return el;
-  //   }
-  // });
-
-  // let DisplayedMovies = filteredMovies.slice(0, 4);
-  // return (
-  //   <>
-  //     <div className="row">
-  //       <div className="col-8 mt-4">
-  //         <table className="table">
-  //           <thead>
-  //             <tr>
-  //               <th scope="col">Title</th>
-  //               <th scope="col">Genre</th>
-  //               <th scope="col">Stock</th>
-  //               <th scope="col">Rate</th>
-  //               <th scope="col">Fav</th>
-  //               <th scope="col"></th>
-  //             </tr>
-  //           </thead>
-  //           <tbody>
-  //             {DisplayedMovies.map((el) => {
-  //               return (
-  //                 <tr key={el._id}>
-  //                   <td>{el.title}</td>
-  //                   <td>{el.genre.name}</td>
-  //                   <td>{el.numberInStock}</td>
-  //                   <td>{el.dailyRentalRate}</td>
-  //                   <td
-  //                     onClick={() => {
-  //                       props.toggleLike(el._id);
-  //                     }}
-  //                   >
-  //                     {el.liked ? (
-  //                       <span className="material-icons-outlined like">
-  //                         favorite
-  //                       </span>
-  //                     ) : (
-  //                       <span className="material-icons-outlined like">
-  //                         favorite_border
-  //                       </span>
-  //                     )}
-  //                   </td>
-  //                   <td>
-  //                     <button onClick={()=>{
-  //                       props.deleteMovie(el._id);
-  //                     }} className="delete-btn">Delete</button>
-  //                   </td>
-  //                 </tr>
-  //               );
-  //             })}
-  //           </tbody>
-  //         </table>
-  //       </div>
-  //     </div>
-  //     <Pagination />
-  //   </>
-  // );
 }
 
 export default Table;
